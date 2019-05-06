8dd8d4e1a4a5c814d8877ea8d627ddfaadfcbf3a
ssaanvi <league@WTS22.attlocal.net>
2019-05-06 16:57:03 -0700

    Monday the 6th of May!!! I JUST TURNED 11

--- src/houses/Houses.java
+++ src/houses/Houses.java
@@ -1,26 +1,72 @@
 package houses;
 
+import java.awt.Color;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
-public static void main(String[] args) {
-	
-	
-	Robot rob = new Robot () ;
-	
-	rob.moveTo(100, 500);
-	rob.penDown();
-	rob.setPenWidth(5);
-	rob.setRandomPenColor();
-	rob.setSpeed(9);
-	rob.move(100);
-	rob.turn(90);
-	rob.move(50);
-	rob.turn(90);
-	rob.move(100);
-	rob.turn(270);
-	rob.move(30);
-	
-	
-}
+	static Robot rob = new Robot();
+	static String small = "60";
+	static String medium = "120";
+	static String large = "250";
+	static int x = Integer.parseInt(small);
+	static int e = Integer.parseInt(medium);
+	static int s = Integer.parseInt(large);
+
+	public static void main(String[] args) {
+		String color = JOptionPane
+				.showInputDialog("Pick a color out of the following choices : Red, blue, green, orange, or pink.");
+		if (color.equalsIgnoreCase("red")) {
+			rob.setPenColor(Color.RED);
+		}
+		if (color.equalsIgnoreCase("orange")) {
+			rob.setPenColor(Color.ORANGE);
+		}
+		if (color.equalsIgnoreCase("blue")) {
+			rob.setPenColor(Color.BLUE);
+		}
+		if (color.equalsIgnoreCase("pink")) {
+			rob.setPenColor(Color.PINK);
+		}
+		if (color.equalsIgnoreCase("green")) {
+			rob.setPenColor(Color.GREEN);
+		}
+
+		rob.moveTo(100, 500);
+		rob.penDown();
+		rob.setPenWidth(3);
+
+		rob.setSpeed(9);
+		Random r = new Random();
+		for (int i = 0; i < 2; i++) {
+			drawFlatRoof();
+		}
+		for (int i = 0; i < 1; i++) {
+			drawPointyRoof();
+		}
+	}
+
+	public static void drawFlatRoof() {
+
+		rob.move(s);
+		rob.turn(90);
+		rob.move(x);
+		rob.turn(90);
+		rob.move(s);
+		rob.turn(270);
+		rob.move(30);
+		rob.turn(270);
+	}
+
+	public static void drawPointyRoof() {
+
+		rob.move(25);
+		rob.turn(70);
+		rob.move(25);
+		rob.turn(50);
+
+	}
 }
